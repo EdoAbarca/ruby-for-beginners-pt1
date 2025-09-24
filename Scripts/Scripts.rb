24a731229266f315df0bbc20bff286d75e15b5e7
EdoAbarca <eduardo.abarca.c@usach.cl>
2025-09-24 18:26:02 -0300

    feat: Basic chapter script written

--- Scripts/Scripts.rb
+++ Scripts/Scripts.rb
@@ -1,4 +1,20 @@
-# Scripts.rb
+# 1 Scripts.rb
 # Basic Ruby scripting examples
 
 puts "Hello, World! This is a Ruby script."
+puts 1+1
+puts "Soprole".upcase
+
+# Can I do something more complex? (From here built by autocompletion)
+def factorial(n)
+  return 1 if n == 0
+  n * factorial(n - 1)
+end
+
+puts "Factorial of 5 is #{factorial(5)}"
+
+#print works too
+print "This is printed using print function.\n" # \n for new line
+
+## pp for pretty print
+pp [1,2,3]
